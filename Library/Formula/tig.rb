50c465d77c2f265a7ffe254fc33b8700706dbce6
Rich Healey <richard.healey@99designs.com>
2012-05-17 17:41:42 +1000

    Preliminary HEAD build for tig
    pulls from my fork to retrieve upstream patches, bails on a missing DTD
    when generating docs

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -5,7 +5,18 @@ class Tig < Formula
   url 'http://jonas.nitro.dk/tig/releases/tig-1.0.tar.gz'
   md5 'a2d414d1cebbc9cd4f3d545bc6f225c6'
 
+  head 'git://github.com/richo/tig.git'
+
+  if ARGV.build_head?
+    depends_on "xmlto" => :build
+    depends_on "docbook" => :build
+  end
+
   def install
+    if ARGV.build_head?
+      system "make configure"
+    end
+
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make install"
     system "make install-doc-man"
