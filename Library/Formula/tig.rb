3d76188aacd60ec4c52024b4ac4b0c2375d01eca
richo <richo@psych0tik.net>
2012-05-18 23:27:12 +1000

    Get the build working for now

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -19,9 +19,9 @@ class Tig < Formula
 
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make install"
-    system "make install-doc-man"
+    # system "make install-doc-man"
 
-    doc.install Dir['*.html']
-    (prefix+'etc/bash_completion.d').install 'contrib/tig-completion.bash'
+    # doc.install Dir['*.html']
+    # (prefix+'etc/bash_completion.d').install 'contrib/tig-completion.bash'
   end
 end
